03edda473b9bace925d5e543960ca6c07e76ce90
Can Wang <canwan@microsoft.com>
2018-08-15 23:06:33 +0800

    add build single component

--- build/build_center.py
+++ build/build_center.py
@@ -8,18 +8,40 @@ class ServiceNode(object):
     def __init__(self, path, service_name):
         self.path = path
         self.service_name = service_name
+        self.pull_docker_files = list()
         self.docker_files = list()
         self.inedges = list()
         self.outedges = list()
 
+
     def dump(self):
-        print ("Path:{}\nName:{}\nInedge:{}\nOutedge:{}".format(
+        print ("Path:{}\nName:{}\nInedge:{}\nOutedge:{}\npull_docker:{}\n".format(
             self.path,
             self.service_name,
             self.inedges,
-            self.outedges
+            self.outedges,
+            self.pull_docker_files
         ))
 
+
+    def build(self):
+        pre_build = os.path.join(self.path, 'build/build-pre.sh')
+        if os.path.exists(pre_build):
+            print ("Pre", pre_build)
+
+        for docker in self.pull_docker_files:
+            print ("Pull", docker)
+
+        for docker in self.docker_files:
+            print ("Build", docker)
+
+        post_build = os.path.join(self.path, 'build/build-post.sh')
+        
+        if os.path.exists(post_build):
+            print ("Post", post_build)
+
+
+        print ('--------------------------')
 
 class ServiceGraph(object):
 
@@ -44,13 +66,35 @@ class ServiceGraph(object):
             self.services[succ_service].inedges.append(prev_service)
         
         else:
-            print ("Error servcies dependency {} to {}.".format(prev_service, succ_service))
+            # print ("Error servcies dependency {} to {}.".format(prev_service, succ_service))
+            self.services[succ_service].pull_docker_files.append(prev_service)
+
+
+    def topology(self):
+        prev_count = dict()
+        ret = list()
+        search_queue = list()
+        for name, node in self.services.items():
+            prev_count[name] = len(node.inedges)
+            if prev_count[name] == 0:
+                search_queue.append(name)
+        
+        while search_queue:
+            current_node = search_queue[0]
+            search_queue.pop(0)
+            ret.append(current_node)
+            for succ_service in self.services[current_node].outedges:
+                prev_count[succ_service] -= 1
+                if prev_count[succ_service] == 0:
+                    search_queue.append(succ_service)
+        return ret
 
 
     def dump(self):
         for _, service in self.services.items():
             service.dump()
 
+    
 def main():
     dirs = "src"
 
@@ -61,7 +105,7 @@ def main():
     for path, dir_list, file_list in g:
         if path == dirs:
             for service in dir_list:
-                graph.add_service(path, service)        
+                graph.add_service(os.path.join(path, service), service)        
         service_name = path.split(os.sep)
         service_name = service_name[-2] if len(service_name) > 1 else None
         for file_name in file_list:
@@ -86,27 +130,10 @@ def main():
                         graph.add_dependency(line.strip(), service_name)
 
     # Build topology sequence
-    def topology(graph):
-        prev_count = dict()
-        ret = list()
-        search_queue = list()
-        for name, node in graph.services.items():
-            prev_count[name] = len(node.inedges)
-            if prev_count[name] == 0:
-                search_queue.append(name)
-        
-        while search_queue:
-            current_node = search_queue[0]
-            search_queue.pop(0)
-            ret.append(current_node)
-            for succ_service in graph.services[current_node].outedges:
-                prev_count[succ_service] -= 1
-                if prev_count[succ_service] == 0:
-                    search_queue.append(succ_service)
-        return ret
-
-    print ("List: {}".format(topology(graph)))
-
+    services = graph.topology()
+    for item in services:
+        graph.services[item].build()
+    
     # graph.dump()
 
 if __name__ == "__main__":
